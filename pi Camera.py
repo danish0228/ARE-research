928291eb355f87c056318533f44abc99a2e92252
Danish <50658650+danish0228@users.noreply.github.com>
2019-12-17 16:22:16 +0900

    Update pi Camera.py

--- pi Camera.py
+++ pi Camera.py
@@ -1,9 +1,19 @@
-import time
+from time improt sleep
 import picamera
+from PIL import Image, ImageDraw, ImageFront
+import datetime
 
-with picamera.PiCamera() as camera:
- camera.resolution = (1024, 768)
- camera.start_preview()
- time.sleep(2)
- camera.capture('my_picture.jpg')
+camera = picamera.PiCamera()
+for i in range(1*60*60*1):
+ sleep(0.5)
+ file_name = "{0:08}.jpg".format(i)
+camera.capture("/home/pi/camera/img/"+file_name)
+font = ImageFront.truetype('/home/pi/camra/OpenSans-Light.ttf',12)
+img = Image.open("/home/pi/camera/img/" + file_name, 'r')
+draw = ImageDraw.Draw(img)
+draw.text((10,10), str(datetime.datetime.now()),front=front, fill='#F00')
+img.save("/home/pi/camera/img/" + file_name)
+print(file_name)
+
+
  
